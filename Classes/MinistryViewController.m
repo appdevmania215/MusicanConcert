b53742c352207020aad18603fd4857188d75a439
Eisen Montalvo <eisen@mac.com>
2010-11-06 08:26:15 -0700

    Removed testing code. Fixed code indentation.

--- Classes/MinistryViewController.m
+++ Classes/MinistryViewController.m
@@ -8,55 +8,31 @@
 
 #import "MinistryViewController.h"
 
-
 @implementation MinistryViewController
 
-/*
- // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
-        // Custom initialization
-    }
-    return self;
-}
-*/
-
-
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad
 {
-	NSLog(@"Here");
     [super viewDidLoad];
-	UITabBarItem* tabItem = [self tabBarItem];
-	NSLog(@"%@", [tabItem badgeValue]);
-	[tabItem setBadgeValue:@"1"];
 }
 
-
-
-// Override to allow orientations other than the default portrait orientation.
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
-- (void)didReceiveMemoryWarning {
-    // Releases the view if it doesn't have a superview.
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
 }
 
-- (void)viewDidUnload {
+- (void)viewDidUnload
+{
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
 }
 
-
-- (void)dealloc {
+- (void)dealloc
+{
     [super dealloc];
 }
-
 
 @end
